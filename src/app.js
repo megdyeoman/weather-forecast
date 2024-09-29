a14786ecf3e187c59adcf2dd6a43ef57f67816c3
Megan <megdyeoman@gmail.com>
2024-09-29 22:35:55 +0100

    Updated project

--- src/app.js
+++ src/app.js
@@ -4,19 +4,19 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windSpeedElement = document.querySelector("#windSpeed");
+  let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  icon.innerHTML =
-    '<img src="${response.data.condition.icon_url}" class="weather-app-icon" />';
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = "${response.data.temperature.humidity}%";
-  windSpeedElement.innerHTML = "${response.data.wind.speed}mph";
+  humidityElement.innerHTML = "${response.data.temperature.humidity} %";
+  windSpeedElement.innerHTML = "${response.data.wind.speed} mph";
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML =
+    '<img src="${response.data.condition.icon_url}" class="weather-app-icon" />';
 }
 
 function formatDate(date) {
